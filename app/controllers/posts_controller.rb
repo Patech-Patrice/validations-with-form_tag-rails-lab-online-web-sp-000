a4a164f90ad554dc01cbaffb461e8f26681d12fc
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-04-28 15:51:51 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
-  def new 
-    @post = Post.new 
-  end 
-  
-  def create 
-    @post = Post.new(post_params)
-    if @post.valid?
-      @post.save
-    redirect_to post_path(@post)
-  else 
-    render :new 
-  end 
-  end 
 
   def edit
     @post = Post.find(params[:id])
@@ -23,12 +9,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    if @post.valid?
+
+    if @post.update(post_params)
+
     redirect_to post_path(@post)
-  else 
-    render :edit 
-  end 
+
+   else 
+
+    render :edit
+   end
   end
 
   private
